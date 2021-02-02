0a1acdf8bfdc367eb6ba7ecf1443ebc342f7fd2c
Joey Haas <josephthaas@gmail.comm>
2021-02-02 08:41:38 -0700

    Created the skeleton of the deck class

---
+++ lib/deck.rb
@@ -0,0 +1,32 @@
+class Deck
+  attr_reader :cards
+
+  def initiatilze(cards)
+    @cards= cards
+  end
+
+  def rank_of_card_at
+
+  end
+
+  def cards
+
+  end
+
+  def high_ranking_cards
+
+  end
+
+  def percent_high_ranking
+
+  end
+
+  def remove_card
+
+  end
+
+  def add_card
+
+  end
+
+end
