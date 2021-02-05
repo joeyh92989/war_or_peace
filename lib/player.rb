809845134143e8a882f2e3db0195a551a88a9b8f
Joey Haas <josephthaas@gmail.com>
2021-02-04 21:15:24 -0700

    created player_has_lost? method

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,14 @@
 class Player
   attr_reader :name,
               :deck
+
   def initialize(name, deck)
     @name = name
     @deck = deck
+    @player_has_lost = false
+  end
+
+  def player_has_lost?
+    @player_has_lost
   end
 end
