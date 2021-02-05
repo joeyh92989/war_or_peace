d6f90369118810d0341f04dd3ced1bc560c118d7
Joey Haas <josephthaas@gmail.com>
2021-02-04 21:10:02 -0700

    created player_has_lost? test case

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,7 @@ class Player
   attr_reader :name,
               :deck
   def initialize(name, deck)
-    @name
-    @deck
+    @name = name
+    @deck = deck
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -1,9 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
 require './lib/card'
-require '.lib/deck'
-require '.lib/player'
+require './lib/player'
 
 
-class PlayerTest < Minitest::Test
+class PlayerTest < MiniTest::Test
   def test_player_exists
 
   card1 = Card.new(:diamond,'Queen', 12)
@@ -12,8 +14,21 @@ class PlayerTest < Minitest::Test
 
   deck = Deck.new([card1,card2,card3])
 
-  player = player.new('Clarisa',deck)
-
+  @player = Player.new('Clarisa',deck)
+#require 'pry'; binding.pry
     assert_instance_of Player, @player
+  end
+
+  def test_player_has_lost?
+
+    card1 = Card.new(:diamond,'Queen',12)
+    card2 = Card.new(:space, '3',3)
+    card3 = Card.new(:heart,'Ace',14)
+
+    deck = Deck.new([card1, card2, card3])
+
+    @player = Player.new('Clarisa',deck)
+
+    assert_equal false, @player.player_has_lost?
   end
 end
