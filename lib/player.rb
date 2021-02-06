a744b94e9b1e0364549180589df6d1876f91d488
Joey Haas <josephthaas@gmail.com>
2021-02-05 17:53:05 -0700

    refactored player has lost method

--- lib/player.rb
+++ lib/player.rb
@@ -6,10 +6,14 @@ class Player
     @name = name
     @deck = deck
     @player_has_lost = false
+
   end
 
   def player_has_lost?
-    @player_has_lost
+    if @deck.cards.length > 0
+      @player_has_lost = false
+    else
+      true
+    end
   end
-
 end
